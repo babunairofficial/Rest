d0bc087c343b0ee1f0a9b7a3f649e465a3a1eaae
Babu Nair <babunairofficial@gmail.com>
2024-05-17 12:40:14 +0530

    Create & add new posts route

--- Rest_Class/index.js
+++ Rest_Class/index.js
@@ -28,9 +28,20 @@ let posts = [ //data for posts
 
 
 //route
-app.get("/posts", (req, res) => {
+app.get("/posts", (req, res) => { //all posts
     res.render("index.ejs", {posts});
-})
+});
+
+app.get("/posts/new", (req,res) => { //new form
+    res.render("new.ejs");
+});
+
+app.post("/posts", (req, res) =>{ //add post
+    let {username, content} = req.body;
+    posts.push({username, content});
+    res.send("post requests working");
+});
+
 
 app.listen(port, () => {
     console.log("listening to port : 8080");
