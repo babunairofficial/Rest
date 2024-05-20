806c0fed5454f633706600cc15e31061ce5a68e8
Babu Nair <babunairofficial@gmail.com>
2024-05-20 17:47:23 +0530

    styling the nav bar

--- Rest_Class/index.js
+++ Rest_Class/index.js
@@ -29,8 +29,8 @@ let posts = [ //data for posts
     },
     {
         id: uuidv4(), // ⇨ '1b9d6bcd-bbfd-4b2d-9b5d-ab8dfbbd4bed'
-        username: "minucherian",
-        content: "I am a doctor",
+        username: "suresh",
+        content: "i love pets",
     },
 ]    
 
